9d1869db46ec2fb2167c4a3a6f2cd4ac934f519c
Daniel Morrison <daniel@collectiveidea.com>
2008-02-28 12:23:19 -0500

    Added validates_chronological for ensuring dates are in order.

--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 require 'core_ext/time'
 require 'calendar_builder'
 require 'active_record/event_finders'
+require 'active_record/event_validations'
 
 ActionView::Base.send :include, CollectiveIdea::CalendarBuilder
 ActiveRecord::Base.send :include, CollectiveIdea::EventFinders
+ActiveRecord::Base.send :include, CollectiveIdea::EventValidations
---
+++ lib/active_record/event_validations.rb
@@ -0,0 +1,26 @@
+module CollectiveIdea
+  module EventValidations
+    
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+    
+    module ClassMethods
+      # Validates date/time fields in chronological order.  
+      def validates_chronological(*attr_names)
+        options = {:allow_nil => true, :message => "is not in chronological order", :on => :save }
+        options.update(attr_names.extract_options!).symbolize_keys
+                
+        send(validation_method(options[:on]), options) do |record|
+          previous = record.send(attr_names.first)
+          attr_names.each do |attr|
+            value = record.send(attr)
+            next if (value.nil? && options[:allow_nil]) || value >= previous
+            record.errors.add(attr, options[:message])
+          end
+        end
+      end
+    end
+
+  end
+end
