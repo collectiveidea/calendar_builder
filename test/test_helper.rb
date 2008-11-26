056102dad778cfd83d75ab88b139df2900aff199
Brandon Keepers <brandon@collectiveidea.com>
2008-11-26 09:40:41 -0600

    Use in-memory sqlite database for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,20 +9,16 @@ require 'active_record'
 require File.dirname(__FILE__) + '/../init.rb'
 
 require 'test/unit'
-require 'ruby-debug'
 require 'mocha'
 require 'action_controller/test_process'
-
 require 'active_record/fixtures'
 
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 
 ActiveRecord::Base.configurations = YAML::load(IO.read(File.dirname(__FILE__) + "/db/database.yml"))
-ActiveRecord::Base.establish_connection(ENV["DB"] || "sqlite3")
+ActiveRecord::Base.establish_connection(ENV["DB"] || "sqlite3mem")
 ActiveRecord::Migration.verbose = false
 load(File.join(File.dirname(__FILE__), "db", "schema.rb"))
-
-Dir["#{File.dirname(__FILE__)}/fixtures/*.rb"].each {|file| require file }
 
 class Test::Unit::TestCase #:nodoc:
   self.fixture_path = File.dirname(__FILE__) + "/fixtures/"
